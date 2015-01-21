4c228dd943d31a8b4e012241caf62e568bc2f356
Timothy Gibbons <tjgibbons@rootmc.com>
2015-01-20 21:36:13 -0600

    IDK
    If this gets deleted ill get mad
    added path

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<?php 
-require 'php/config.php';
+<?php
+$path ="/";
+require $path.'php/config.php';
 ?>
 <html>
 	<head>
 		<title>
 			<?php echo $config['SERVERNAME']."&bull; Home"; ?>
 		</title>
-		<?php include 'asset/includes/css.php'; ?>
+		<?php include $path.'asset/includes/css.php'; ?>
 	</head>
 	<body>
 		<div class="body">
 			THIS IS SOME TEXT
 		</div>
-		<?php include 'asset/includes/scripts.php';?>
+		<?php include $path.'asset/includes/scripts.php';?>
 	</body>
 </html>
