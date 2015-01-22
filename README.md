49b0431ca12b173abf9270a70c70e93a2d749f95
Timothy Gibbons <tjgibbons@rootmc.com>
2015-01-22 06:41:27 -0600

    Work
    almost redone the whole register process

--- README.md
+++ README.md
@@ -6,6 +6,3 @@ THIS IS STILL IN DEVOPEMENT JUST HAVE THIS AS A BACKUP
 Right now builds are broken feel free to pull files and send them to me.
 
 Tester are welcome!
-
-MCServer Website: http://rootmc.com
-MCServer IP: play.rootmc.com
--- pages/register/index.php
+++ pages/register/index.php
@@ -1,6 +1,7 @@
 <?php 
 	$path = "../../";
 	require $path.'php/config.php';
+	
 ?>
 <html>
 	<head>
@@ -11,7 +12,46 @@
 		<!-- TODO: NAV -->
 		<div class="container">
 			<div class="row">
-			<form action="register.php" method="post">
+			<?php 
+			$FNAME = $_POST['FirstName'];
+			$LNAME = $_POST['LastName'];
+			$UNAME = $_POST['UName'];
+			$EMAIL = $_POST['EMAIL'];
+			$CEMAIL = $_POST['CEmail'];
+			$MCUSER = $_POST['MCUSER'];
+			$PASS = $_POST['Password'];
+			$CPASS = $_POST['CPass'];
+			
+			$hash = hash('sha256',$PASS);
+			//TODO: Make a if(Cpass == Pass)
+			//TOTO: ReDO if State ments
+			/*
+			 * but, if the user doesn't enter an email and a password, for example
+				[6:30:23 AM] Sam: it'll only display the error for the email
+			 */
+			if(($FNAME !="") && ($LNAME !="") && ($UNAME !="") && ($EMAIL !="") && ($CEMAIL !="") && ($MCUSER !="") && ($PASS !="") && ($CPASS !=""))
+			{
+				addUser($FNAME, $LNAME, $UNAME,$EMAIL, $MCUSER, $PASS);
+				
+			}else if(($FNAME == "")){
+				echo "<div class='alert alert-danger'>You must enter a <strong>first</strong> name!</div>";
+			}else if($LNAME == ""){
+				echo "<div class='alert alert-danger'>You must enter a <strong>last</strong> name!</div>";
+			}else if($UNAME == ""){
+				echo "<div class='alert alert-danger'>You must enter a <strong>username</strong>!</div>";
+			}else if($EMAIL == ""){
+				echo "<div class='alert alert-danger'>You must enter a <strong>Email</strong>!</div>";
+			}else if($CMAIL == ""){
+				echo "<div class='alert alert-danger'>You must confirm your <strong>Email</strong>!</div>";
+			}else if($MCUSER == ""){
+				echo "<div class='alert alert-danger'>You must hava a <strong>Paied Minecraft User Account</strong>!</div>";
+			}else if($PASS == ""){
+				echo "<div class='alert alert-danger'>You must enter a <strong>Password</strong>!</div>";
+			}else if($CPASS == ""){
+				echo "<div class='alert alert-danger'>You must confirm your <strong>Password</strong>!</div>";
+			}
+			?>
+			<form action="" method="post">
 					<div class="form-group">
 						<input type="text" name="FirstName" id="FirstName "placeholder="FirstName"/>
 					</div>
