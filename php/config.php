666fa346a41e4462d8cead253ac64d2c825c3f1e
Timothy Gibbons <newtj599zzzbro@users.noreply.github.com>
2015-01-31 11:52:56 -0600

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@ if (file_exists($path."install/index.php")){
 	// whatever you'll do, eg die();
 	die("Please pages/install/index.php When you set up and finish useing the file!");
 }else{
-
+//TODO: MAKE SETUP CHANGE: $mysql,$mySqlTables, $config, $infraction, $downtime
 //Enter Your MySql information here to use the forums or other stuff
 $mySql = array(
 		"HOST"=>"localhost",
