c157aab1f5b63e6b3a83b8ff4eb274ada70fc6ac
Timothy Gibbons <tjgibbons@rootmc.com>
2015-02-01 10:58:00 -0600

    Add play area
    This is where all the players online are stored and other stuff

---
+++ pages/play/index.php
@@ -0,0 +1,33 @@
+<?php
+	$path = "../../";
+	require $path.'php/config.php';
+?>
+<html>
+	<head>
+		<title><?php echo $config['SERVERNAME'];>?>	&bull; PLAY </title>	
+	</head>
+	<body>
+		<div class="main">
+			<div class="container">
+				<div class="jumbotronara">
+					<div class="jumbotron">
+						<h1>Welcome to <?php echo $config['SERVERNAME'];?></h1>
+					</div>
+				</div>
+				<div class="serverinfo">
+					<div class="server-info">
+						<div class="panel panel-info">
+							<div class="panel-heading">
+							</div>
+							<div class="panel-body">
+							</div>
+						</div>
+					</div>
+					<div class="players">
+						<?//TODO: MAKE A PLAYERS AREA ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
