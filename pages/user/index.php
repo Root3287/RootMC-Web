b498dc4c7a45bb93da2fe0e15b851d3340f436e8
Timothy Gibbons <tjgibbons@rootmc.com>
2015-02-01 17:30:53 -0600

    CP
    added control panels

---
+++ pages/user/index.php
@@ -0,0 +1,18 @@
+<?php
+if(!(isset($_GET['page'])){
+$_GET['page']=="user";
+}else{
+	switch($_GET['page']){
+		case "user":
+		include 'user/user.php';
+		break;
+		
+		case "admin":		
+		include 'admin/admin.php';
+		break;
+		
+		case "mod":
+		include "mod/mod.php";
+		break;
+	}
+}
