40e9ed6b82c84429d5e6fd1d8b02ebd50e2f30c0
Timothy Gibbons <Root3287@users.noreply.github.com>
2015-03-06 17:34:15 -0600

    Update forums.class.php

--- php/classes/forums.class.php
+++ php/classes/forums.class.php
@@ -1,9 +1,7 @@
 <?php 
-	require 'db.class.php';
-	class forum{
+	class forums{
 		public $db;
 		public function __construct(){
-			$this->db = new db();
 			$this->db = $this->db->getInstance();
 		}
 		public function newTopic($catID, $title, $content, $author){
@@ -11,6 +9,21 @@
 		}
 		public function newReply($tid, $title, $content, $author){
 			//$this->db->query("INSERT INTO reply()")
+		}
+		public function getCat(){
+			$q = $this->db->query("SELECT * FROM category");
+			$result = "";
+			while($row = $q->fetch_assoc()){
+				if($row['Parent'] == 1){
+					if($row['Parent_ID'] < 0){
+						$result[$row['ID']] = $row
+					}
+				}else{
+					if($row['Parent_Id'] > 0){
+						$result[$row['ID']] = $row;
+					}
+				}
+			}
 		}
 	}
 ?>
