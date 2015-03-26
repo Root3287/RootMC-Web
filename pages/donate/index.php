112560947538044d576337644177b4f5d3e84979
Timothy Gibbons <Root3287@users.noreply.github.com>
2015-03-26 07:21:29 -0500

    Pages Update

--- pages/donate/index.php
+++ pages/donate/index.php
@@ -1,2 +1,56 @@
 <?php
+$page ="Donate";
+$path ="../../";
+require $path.'php/config.php';
 ?>
+<html>
+	<head>
+		<title>
+			<?php echo $CONFIG['SERVERNAME']." &bull; ". strtoupper($page); ?>
+		</title>
+		<?php include $path.'asset/includes/css.php'; ?>
+	</head>
+	<body class="home">
+		
+		<div class="main">
+			<div class="nav">
+				<?php include $path.'asset/includes/nav.php';?>
+			</div>
+			<div class="container">
+					<div class="jumbotron">
+					<h1>Welcome to <?php echo $CONFIG['SERVERNAME'];?></h1>
+					<h4><?php echo $CONFIG['SERVERIP'];?></h4>
+				</div>
+				<div class="home_container">
+						<div class="news">
+							<h2>News</h2>
+							<div class="panel panel-bg">
+								<div class="panel-heading">
+									NEWS
+								</div>
+								<div class="panel-body">
+										TODO:This is where the news goes to 
+									</div>
+								</div>
+							</div>
+							
+						<div class="login">
+							<h2>Login</h2>
+							<div class="panel panel-bg">
+								<div class="panel-heading">
+										<?php if(isset($_COOKIE['user'])){echo $_COOKIE['user'];}else{?>REGISTER/LOGINS<?php } ?>
+								</div>
+								<div class="panel-body">
+										TODO:This is where the Login goes to 
+									</div>
+								</div>
+						</div>
+						
+				</div>
+			</div>
+				<?php include $path.'asset/includes/footer.php';?>
+			</div>
+			
+		<?php include $path.'asset/includes/scripts.php';?>
+	</body>
+</html>
