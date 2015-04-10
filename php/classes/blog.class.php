81db29625a2aee5c4a2bc587cba687bc83ac5a12
Timothy Gibbons <Root3287@users.noreply.github.com>
2015-04-10 07:29:25 -0500

    Add some more code

--- php/classes/blog.class.php
+++ php/classes/blog.class.php
@@ -5,15 +5,24 @@ class blog{
 		$this->db = db::getInstance;
 	}
 	public function getPost(){
+		$posts = $this->db->query("SELECT * FROM blog")->result();
+		$return = array();
+		$n = 0;
+		foreach($posts as $post){
+			$return[$n]['Title'] = $post['Title'];
+			$return[$n]['Content'] = $post['Content'];
+			$return[$n]['Date'] = $post['Date'];
+			$return[$n]['Author'] = $post['Author'];
+			$n++;
+		}
+	}
+	public function createNew($title, $content, $author){
 		
 	}
-	public function createNew(){
+	public function deletePost($id){
 		
 	}
-	public function deletePost(){
-		
-	}
-	public function editPost(){
+	public function editPost($id, $title, $content){
 		
 	}
 }
