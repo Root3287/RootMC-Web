9647e5912c9ed3b0711fa732ad69bf23c7502d63
Timothy Gibbons <tjgibbons@rootmc.com>
2015-04-18 18:41:41 -0500

    Update Footer and license

--- asset/includes/footer.php
+++ asset/includes/footer.php
@@ -1,5 +1,3 @@
-<nav class="navbar navbar-default navbar-fixed-bottom">
   <div class="container">
   	<a href="/pages/License.php">License</a>&emsp;Copyright &copy; Timothy Gibbons;&nbsp;<a href="/pages/credits.php">Credits</a>
   </div>
-</nav>
