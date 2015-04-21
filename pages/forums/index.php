e5d6b1f852e1b963342102b5f620c26fc64f2b3d
Timothy Gibbons <Root3287@users.noreply.github.com>
2015-04-21 07:20:46 -0500

    Update index.php

--- pages/forums/index.php
+++ pages/forums/index.php
@@ -5,7 +5,7 @@ require path.'php/init.php';
 require path.'php/classes/forums.class.php';
 $user = new user();
 $forums = new forums();
-$cats = $forums->getCat($user->getGroupID());
+$cats = $forums->getCat($user->getRank());
 ?>
 <html>
 	<head>
