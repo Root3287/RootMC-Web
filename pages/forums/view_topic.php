6b3674fc33f2fc5093a3ca81d6b35b79f4377f03
Timothy Gibbons <Root3287@users.noreply.github.com>
2015-04-23 07:29:00 -0500

    Update view_topic.php

--- pages/forums/view_topic.php
+++ pages/forums/view_topic.php
@@ -5,12 +5,7 @@ $forums = new forums();
 if(!isset($_GET['cid']) && !isset($_GET['tid'])){
 	Redirect::to('index.php');
 }
-$topics = $forums->getTopic();
-foreach($topics as $topic){
-	$title = $topic['Title'];
-	$content = $topic['Content'];
-	$author = $topic['Author'];
-}
+$topics = $forums->getTopic($tid);
 ?>
 <html>
 	<head>
@@ -23,38 +18,51 @@ foreach($topics as $topic){
 			<?php include path.'asset/includes/nav.php';?>
 			<div class="container">
 				<div class="op">
+					<?php
+					foreach($topics as $topic){
+					?>
 					<div class="panel panel-default">
 						<div class="panel-heading">
-							title
+							<?php echo $topic->Title; ?>
 						</div>
 						<div class="panel-body">
 							<div class="author">
-								SOMEBODY
+								<?php echo $topic->Author; ?>
 							</div>
 							<div class="information">
-								this is my oppion
+								<?php echo $topic->Content;?>
 							</div>
 						</div>
 					</div>
+					<?php
+					}
+					?>
+				</div>
+				
+				<div class="reply">
+					<?php
+					foreach($replies as $reply){
+					?>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<?php echo $reply->Title;?>
+						</div>
+						<div class="panel-body">
+							<div class="author">
+								<?php echo $reply->Author;?>
+							</div>
+							<div class="information">
+								<?php echo $reply->Content;?>
+							</div>
+						</div>
+					</div>
+					<?php
+					}
+					?>
 				</div>
 				<div class="cats">
 					<div class="well">
 						<div class="container">
-						</div>
-					</div>
-				</div>
-				<div class="reply">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							title
-						</div>
-						<div class="panel-body">
-							<div class="author">
-								SOMEBODY
-							</div>
-							<div class="information">
-								this is my oppion
-							</div>
 						</div>
 					</div>
 				</div>
